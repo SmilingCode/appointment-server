ae308d67adb1e4e6281c092be8d70203c0498382
SmilingCode <ericc_j@icloud.com>
2019-10-04 01:05:41 +1000

    basic structure

--- app.js
+++ app.js
@@ -1,8 +1,6 @@
 var createError = require('http-errors');
 var express = require('express');
 var path = require('path');
-var cookieParser = require('cookie-parser');
-var logger = require('morgan');
 
 var indexRouter = require('./routes/index');
 var usersRouter = require('./routes/users');
@@ -13,14 +11,66 @@ var app = express();
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'jade');
 
-app.use(logger('dev'));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
-app.use(cookieParser());
+
 app.use(express.static(path.join(__dirname, 'public')));
 
 app.use('/', indexRouter);
 app.use('/users', usersRouter);
+
+// Returns an array of all days in the specified month
+app.get('/days', function(req, res) {
+  let resMsg = {
+    success: null,
+    days: []
+  }
+
+  const year = req.query.year;
+  const month = req.query.month;
+
+  // query google calender
+  
+  res.send(resMsg);
+});
+
+// Returns a list of all 40-minute time slots available
+app.get('timeslots', function(req, res) {
+  let resMsg = {
+    success: null,
+    timeSlots: []
+  }
+
+  const year = req.query.year;
+  const month = req.query.month;
+  const day = req.query.day;
+
+  // query google calender
+
+  res.send(resMsg);
+});
+
+app.post('book', function (req, res) {
+  let resMsg = {
+    success: null,
+    startTime: null,
+    endTime: null
+  }
+
+  let errMsg = {
+    success: null,
+    message: null
+  }
+
+  const year = req.query.year;
+  const month = req.query.month;
+  const day = req.query.day;
+  const hour = req.query.hour;
+  const minute = req.query.minute;
+
+  // query google calender
+  
+})
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
